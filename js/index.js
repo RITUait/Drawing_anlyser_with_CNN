7bb2032aa5f8ebdf46c984f12263b70bfca6b995
abhilashreddyy <abhilashreddyy1@gmail.com>
2018-02-22 17:50:03 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,106 @@
+var ctx, color = "#000";
+
+document.addEventListener( "DOMContentLoaded", function(){
+
+	// setup a new canvas for drawing wait for device init
+    setTimeout(function(){
+	   newCanvas();
+    }, 1000);
+
+}, false );
+
+// function to setup a new canvas for drawing
+function newCanvas(){
+	//define and resize canvas
+  document.getElementById("content").style.height = window.innerHeight-90;
+  var canvas = '<canvas id="canvas" width="'+window.innerWidth+'" height="'+(window.innerHeight-90)+'"></canvas>';
+	document.getElementById("content").innerHTML = canvas;
+
+    // setup canvas
+	ctx=document.getElementById("canvas").getContext("2d");
+	ctx.strokeStyle = color;
+	ctx.lineWidth = 5;
+
+	// setup to trigger drawing on mouse or touch
+  drawTouch();
+  drawPointer();
+	drawMouse();
+}
+
+function selectColor(el){
+    for(var i=0;i<document.getElementsByClassName("palette").length;i++){
+        document.getElementsByClassName("palette")[i].style.borderColor = "#777";
+        document.getElementsByClassName("palette")[i].style.borderStyle = "solid";
+    }
+    el.style.borderColor = "#fff";
+    el.style.borderStyle = "dashed";
+    color = window.getComputedStyle(el).backgroundColor;
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+}
+
+// prototype to	start drawing on touch using canvas moveTo and lineTo
+var drawTouch = function() {
+	var start = function(e) {
+		ctx.beginPath();
+		x = e.changedTouches[0].pageX;
+		y = e.changedTouches[0].pageY-44;
+		ctx.moveTo(x,y);
+	};
+	var move = function(e) {
+		e.preventDefault();
+		x = e.changedTouches[0].pageX;
+		y = e.changedTouches[0].pageY-44;
+		ctx.lineTo(x,y);
+		ctx.stroke();
+	};
+    document.getElementById("canvas").addEventListener("touchstart", start, false);
+	document.getElementById("canvas").addEventListener("touchmove", move, false);
+};
+
+// prototype to	start drawing on pointer(microsoft ie) using canvas moveTo and lineTo
+var drawPointer = function() {
+	var start = function(e) {
+        e = e.originalEvent;
+		ctx.beginPath();
+		x = e.pageX;
+		y = e.pageY-44;
+		ctx.moveTo(x,y);
+	};
+	var move = function(e) {
+		e.preventDefault();
+        e = e.originalEvent;
+		x = e.pageX;
+		y = e.pageY-44;
+		ctx.lineTo(x,y);
+		ctx.stroke();
+    };
+    document.getElementById("canvas").addEventListener("MSPointerDown", start, false);
+	document.getElementById("canvas").addEventListener("MSPointerMove", move, false);
+};
+
+// prototype to	start drawing on mouse using canvas moveTo and lineTo
+var drawMouse = function() {
+	var clicked = 0;
+	var start = function(e) {
+		clicked = 1;
+		ctx.beginPath();
+		x = e.pageX;
+		y = e.pageY-44;
+		ctx.moveTo(x,y);
+	};
+	var move = function(e) {
+		if(clicked){
+			x = e.pageX;
+			y = e.pageY-44;
+			ctx.lineTo(x,y);
+			ctx.stroke();
+		}
+	};
+	var stop = function(e) {
+		clicked = 0;
+	};
+  document.getElementById("canvas").addEventListener("mousedown", start, false);
+	document.getElementById("canvas").addEventListener("mousemove", move, false);
+	document.addEventListener("mouseup", stop, false);
+};
